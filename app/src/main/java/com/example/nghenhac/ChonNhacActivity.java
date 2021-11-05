d28a23aad95a02073a1ffb220bc20d4366e95cdc
hungp1601 <59797651+hungp1601@users.noreply.github.com>
2021-11-05 09:46:04 +0700

    Initial commit

---
+++ app/src/main/java/com/example/nghenhac/ChonNhacActivity.java
@@ -0,0 +1,91 @@
+package com.example.nghenhac;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class ChonNhacActivity extends AppCompatActivity {
+    Button bai1,bai2,bai3,bai4,bai5,bai6,chon;
+    TextView ID;
+    boolean check=false;
+    private static final int RC_VIDEO_APP_PERM = 124;
+    int uri;
+    String name="";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chon_nhac);
+        ID=findViewById(R.id.idbai);
+        bai1=findViewById(R.id.bai1);
+        bai2=findViewById(R.id.bai2);
+        bai3=findViewById(R.id.bai3);
+        bai4=findViewById(R.id.bai4);
+        bai5=findViewById(R.id.bai5);
+        bai6=findViewById(R.id.bai6);
+        chon=findViewById(R.id.chon);
+
+        chon.setOnClickListener(v->{
+            if(check){
+                Intent intent=new Intent(ChonNhacActivity.this,MainActivity.class);
+                Toast.makeText(this,String.valueOf(uri),Toast.LENGTH_SHORT).show();
+                intent.putExtra("name",name);
+                intent.putExtra("ID",ID.getText().toString());
+                intent.putExtra("path",String.valueOf(uri));
+                startActivity(intent);
+                finish();
+            }
+            else{
+                Toast.makeText(ChonNhacActivity.this,"Hay chon bai truoc",Toast.LENGTH_SHORT).show();
+            }
+        });
+        bai1.setOnClickListener(v->{
+            check=true;
+            ID.setText(bai1.getText().toString());
+            uri = R.raw.caooc20;
+            name="cao oc 20";
+        });
+        bai2.setOnClickListener(v->{
+            check=true;
+            ID.setText(bai2.getText().toString());
+            uri = R.raw.castleonthehill;
+            name="castle on the hill";
+        });
+        bai3.setOnClickListener(v->{
+            check=true;
+            ID.setText(bai3.getText().toString());
+            uri = R.raw.dive;
+            name="dive";
+        });
+        bai4.setOnClickListener(v->{
+            check=true;
+            ID.setText(bai4.getText().toString());
+            uri = R.raw.idontcare;
+            name="i dont care";
+        });
+        bai5.setOnClickListener(v->{
+            check=true;
+            ID.setText(bai5.getText().toString());
+            uri = R.raw.nevergonnagiveyouup;
+            name="never gonna give you up";
+        });
+        bai6.setOnClickListener(v->{
+            check=true;
+            ID.setText(bai6.getText().toString());
+            uri = R.raw.shivers;
+            name="shivers";
+        });
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/nghenhac/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.nghenhac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button chonbai,play,pause;
+    TextView tenbai,idbai,thoigian;
+    String ID="",name="",tmp="";
+    int path;
+    private MediaPlayer mediaPlayer;
+    boolean isPause=true;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        chonbai=findViewById(R.id.chonbai);
+        play=findViewById(R.id.play);
+        pause=findViewById(R.id.pause);
+        tenbai=findViewById(R.id.tenbai);
+        idbai=findViewById(R.id.idbai);
+        thoigian=findViewById(R.id.time);
+        if(getIntent().getExtras().get("ID")!=null){
+            ID=getIntent().getExtras().get("ID").toString();
+            name=getIntent().getExtras().get("name").toString();
+            tmp=getIntent().getExtras().get("path").toString();
+
+        }
+        else {
+            tmp="";
+            ID="";
+            name="";
+        }
+        path=Integer.parseInt(tmp);
+
+
+        if(!ID.equals("")){
+            idbai.setText("bai so "+ID);
+            tenbai.setText(name);
+        }
+
+        chonbai.setOnClickListener(v->{
+           Intent intent = new Intent(MainActivity.this,ChonNhacActivity.class);
+           startActivity(intent);
+           finish();
+        });
+
+        play.setOnClickListener(v->{
+            if(!ID.equals("")){
+                Toast.makeText(MainActivity.this,tmp,Toast.LENGTH_SHORT).show();
+                mediaPlayer= MediaPlayer.create(this,path);
+            }
+            else{
+                Toast.makeText(MainActivity.this,"hay chon bai",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        pause.setOnClickListener(v->{
+
+            if(!ID.equals("")){
+
+                mediaPlayer.pause();
+
+            }
+            else{
+                Toast.makeText(MainActivity.this,"hay chon bai",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
